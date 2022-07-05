6670a2beabfd7107e5c4f0a60a16fe1b9da33ccc
maxgate <kelechivincy@gmail.com>
2022-07-05 08:31:09 -0700

    task 4 fix commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-/
+
+/**
 * main - Entry point
 * Return: Always 0 (sucess)
 */
+
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);

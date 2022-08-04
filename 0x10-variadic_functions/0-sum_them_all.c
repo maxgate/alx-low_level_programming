eda64ea1bdeb318c5cc3fafe45f869aa3d5c5058
maxgate <kelechivincy@gmail.com>
2022-08-04 15:16:01 -0700

    task 0 commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,15 +10,15 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum = 0;
 	unsigned int i;
+	int sum = 0;
 	va_list args;
 	
 	va_start(args, n);
 	
 	for (i = 0; i < n; i++)
-		sum += va_arg (args, int);
-	va_end (args);
+		sum += va_arg(args, int);
+	va_end(args);
 
 	return (sum);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,16 +2,20 @@
 #define MAIN_H
 
 #include <stdarg.h>
-
+/**
+ * struct print - print type with corresponding print function
+ * @t: print type
+ * @f: print function
+ */
 typedef struct print
 {
 	char *t;
 	void (*f)(va_list);
 } print_t;
 
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
+int sum_them_all(const unsigned int, ...);
+void print_numbers(const char *, const unsigned int, ...);
+void print_strings(const char *, const unsigned int, ...);
+void print_all(const char * const, ...);
 
 #endif

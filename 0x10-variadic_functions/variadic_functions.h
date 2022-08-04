6143bb3c7ed0f3ecf640c6207fa1e240396c0ce6
maxgate <kelechivincy@gmail.com>
2022-08-04 10:17:16 -0700

    task 0 fix

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+
+typedef struct print
+{
+	char *t;
+	void (*f)(va_list);
+} print_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
